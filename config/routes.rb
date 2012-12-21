a0ae00114e479dfd083c8998ca9029660c2b9ea0
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-21 11:54:58 -0800

    add content pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RailsRecurlySubscriptionSaas::Application.routes.draw do
+  get "content/silver"
+
+  get "content/gold"
+
+  get "content/platinum"
+
   authenticated :user do
     root :to => 'home#index'
   end
