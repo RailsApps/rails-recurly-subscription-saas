390a1a5aeea072a7ee3d14e49703bff5ba7bb74f
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-21 11:23:54 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRecurlySubscriptionSaas::Application.config.secret_token = '191bf13df5bd1b7f521284b9906784f325d86a8747f7c8e23f9029a0a84114f68f69889f9651b75d71e341fdf0c5917cf8fb463050b14d347e4b2df6c43b04a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsRecurlySubscriptionSaas::Application.config.session_store :cookie_store, key: '_rails-recurly-subscription-saas_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsRecurlySubscriptionSaas::Application.config.session_store :active_record_store
