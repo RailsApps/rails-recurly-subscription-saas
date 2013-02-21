1a841c1b23f95521aa70cb9d713683a031736fed
Daniel Kehoe <daniel@danielkehoe.com>
2013-02-20 16:25:59 -0800

    build using rails_apps_composer 2.2.34

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ RailsRecurlySubscriptionSaas::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'example.com' }
 
